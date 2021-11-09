ed0da7c02f9a8cfca3bba2f4797753ead812fe6e
sankey08 <wadjesanket@gmail.com>
2021-11-09 20:20:14 +0530

    First Commit

---
+++ Practice/program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ cout<<sizeof(int)<<endl;
+ cout<<sizeof(double)<<endl;
+ cout<<sizeof(float)<<endl;
+ cout<<sizeof(bool)<<endl;
+ cout<<sizeof(char)<<endl;
+ cout<<sizeof(wchar_t)<<endl;
+}
