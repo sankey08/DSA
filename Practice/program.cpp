18dfea645736e184633da058ccc8d3bb214b91e1
sankey08 <wadjesanket@gmail.com>
2021-11-28 20:50:37 +0530

    Day0

--- Practice/program.cpp
+++ Practice/program.cpp
@@ -1,11 +1,48 @@
 #include<iostream>
 using namespace std;
 
+extern int a,b,c;
+
+int g = 5; //global var
+
+static int count = 10;
+
 int main(){
- cout<<sizeof(int)<<endl;
- cout<<sizeof(double)<<endl;
- cout<<sizeof(float)<<endl;
- cout<<sizeof(bool)<<endl;
- cout<<sizeof(char)<<endl;
- cout<<sizeof(wchar_t)<<endl;
+
+// Data Types
+//  cout<<sizeof(int)<<endl;
+//  cout<<sizeof(double)<<endl;
+//  cout<<sizeof(float)<<endl;ṇ
+//  cout<<sizeof(bool)<<endl;
+//  cout<<sizeof(char)<<endl;
+//  cout<<sizeof(wchar_t)<<endl;
+
+// Enum Type
+// enum color {red,green,blue,yellow} ;
+
+// Redeclaring/using variable 
+// int a = 20,b=25,c=30;
+// cout<<a+b+c;
+
+//LOcal var value will always take preference
+// int g = 10;
+
+//Static Members/Variable
+// int i =5;
+// while(count--){
+//     i++;
+//     cout<<" i is "<<i<<endl;
+//     cout<<"count is "<<count<<endl;
+// }
+
+//Operators
+// cout<<sizeof(a)<<endl;
+// int n = 9;
+// int res = (n==9)?1:0;
+// cout<<res<<endl;
+// int j = (n+2,n*5,n-10);
+// cout<<j<<endl;
+
+
+
 }
