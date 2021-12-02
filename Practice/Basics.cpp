caa37f39050b2930c64087e7c0fc3ca70b28e118
sankey08 <wadjesanket@gmail.com>
2021-12-02 20:59:35 +0530

    Basics Done

--- Practice/Basics.cpp
+++ Practice/Basics.cpp
@@ -138,6 +138,19 @@ C++ provides following two types of string representations −
 
 */
 
+/*
+
+References are often confused with pointers but three major differences between references and pointers are −
+
+    1)You cannot have NULL references. You must always be able to assume that a reference is connected to a legitimate piece of storage.
+
+    2)Once a reference is initialized to an object, it cannot be changed to refer to another object. 
+    Pointers can be pointed to another object at any time.
+
+    3)A reference must be initialized when it is created. Pointers can be initialized at any time.
+
+*/
+// Struct & typedef
 
 }
 
